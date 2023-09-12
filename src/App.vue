6f1aee1c72925db586d287decc1c0532db3ae272
lujiamei02 <lujiamei02@meituan.com>
2023-09-12 09:45:17 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+<!--    <nav>-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link> |-->
+<!--      <router-link to="/entrancePage">Entrance</router-link> -->
+<!--    </nav>-->
     <router-view/>
   </div>
 </template>
@@ -30,3 +31,5 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup>
+</script>
